2d475eea2a8bc6fcb679ec3c23dea0e384e58d29
Geovani <geovani.020204@gmail.com>
2025-04-28 17:03:36 -0400

    Card unfinished

--- src/components/ui/CardField/index.tsx
+++ src/components/ui/CardField/index.tsx
@@ -1,23 +1,77 @@
+import { useState, useEffect } from "react";
+import { LinearProgress, LinearProgressProps, Box } from "@mui/material";
+
 import "./Card.css";
 
-function CardField() {
+type CardProps = {
+  title: string;
+  subtitle?: string;
+  content?: string;
+  tasks?: { id: number; content?: string; done: boolean; cardId: number }[];
+};
+
+function ProgressBar(props: LinearProgressProps & { value: number }) {
   return (
-    <div className="border w-80 shadow-lg rounded-lg p-2">
-      <div className="border grid grid-cols-2">
+    <Box sx={{ width: "100%", mr: 1 }}>
+      <LinearProgress
+        variant="determinate"
+        color="inherit"
+        className="p-1 rounded-sm color"
+        {...props}
+      />
+    </Box>
+  );
+}
+
+function CardField({ title, content, subtitle, tasks }: CardProps) {
+  const [progressBar, setProgressBar] = useState(0);
+  const [progress, setProgress] = useState("");
+  const [screeTask, setScreenTask] = useState("");
+
+  const handleProgressBar = () => {
+    let taskDone: number = 0;
+    tasks!.map((task) => {
+      if (task.done === true) {
+        taskDone++;
+      }
+    });
+
+    let percentage: any = (taskDone * 100) / tasks!.length;
+    setProgressBar(Number(parseInt(percentage)));
+
+    if (percentage > 0) {
+      setProgress("Em progresso");
+    } else if (percentage == 100) {
+      setProgress("Feito");
+    } else {
+      setProgress("Na fila");
+    }
+  };
+
+  const handleTaskScreen = () => {};
+
+  useEffect(() => {
+    handleProgressBar();
+    handleTaskScreen();
+  }, [tasks]);
+  return (
+    <div className="border border-gray-200 w-80 shadow-lg rounded-lg p-2">
+      <div className=" grid grid-cols-2">
         <div>
-          <p>Titulo</p>
-          <p>Subtitulo</p>
-          <p>Em progresso</p>
+          <p>{title}</p>
+          <p>{subtitle}</p>
+          <p>{progress}</p>
         </div>
-        <div className="max-h-24 overflow-y-auto">
-          Resumopra cararlho muito resumo mesmo, tipo resumo demais até muita
-          coisa se não a gente vai se foder filho da puta do caralho arrombado
-          de merda
-        </div>
+        <div className="max-h-24 overflow-y-auto">{content}</div>
       </div>
 
-      <div className="">ProgressBar</div>
-      <div>Taks:</div>
+      <div className="mt-2">
+        <ProgressBar value={progressBar} />
+      </div>
+      <div className="flex justify-between">
+        <span>{screeTask}</span>
+        <span>{progressBar}%</span>
+      </div>
     </div>
   );
 }
--- src/pages/ComponentsUsage/index.tsx
+++ src/pages/ComponentsUsage/index.tsx
@@ -2,12 +2,44 @@ import { useRef } from "react";
 import InputField from "../../components/ui/InputField";
 import CardField from "../../components/ui/CardField";
 
+const arr = [
+  {
+    id: 1,
+    content: "alguma",
+    done: false,
+    cardId: 6,
+  },
+  {
+    id: 2,
+    content: "coisinha",
+    done: false,
+    cardId: 6,
+  },
+  {
+    id: 3,
+    content: "taskinho",
+    done: false,
+    cardId: 6,
+  },
+  {
+    id: 4,
+    content: "La taska",
+    done: false,
+    cardId: 6,
+  },
+];
+
 function ComponentsUsage() {
   const inputRef = useRef<HTMLInputElement>(null);
 
   return (
     <div className="p-16">
-      <CardField />
+      <CardField
+        title="Titulinho"
+        content="Conteudo pra caralho taligado mermão?"
+        subtitle="Subititulinho"
+        tasks={arr}
+      />
     </div>
   );
 }
