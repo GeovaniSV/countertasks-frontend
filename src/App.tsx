252dafc6810ca7071e721da27eb3ab33dbea0f79
Geovani Dos Santos Vargas <geovani.020204@gmail.com>
2025-05-08 19:56:33 -0400

    Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import SideBar from './components/SideBar'
+// import SideBar from './components/SideBar'
 import ComponentsUsage from './pages/ComponentsUsage'
+import Login from './pages/Login'
 
 function App() {
 	return (
 		<div className="flex">
-			<SideBar />
+			{/* <SideBar /> */}
 			<BrowserRouter>
 				<Routes>
 					<Route
-						path="/"
+						path="/home"
 						element={<ComponentsUsage />}
+					/>
+					<Route
+						path="/login"
+						element={<Login />}
 					/>
 				</Routes>
 			</BrowserRouter>
