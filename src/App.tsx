8435a56a5dc030847dfff80248f09c8be9cfedb4
Geovani Dos Santos Vargas <geovani.020204@gmail.com>
2025-05-08 20:31:18 -0400

    app.tsx merge conflicts fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-// import SideBar from './components/SideBar'
+import SideBar from './components/SideBar'
 import ComponentsUsage from './pages/ComponentsUsage'
-import Login from './pages/Login'
-import Register from './pages/Register'
 
 function App() {
 	return (
 		<div className="flex">
-			{/* <SideBar /> */}
+			<SideBar />
 			<BrowserRouter>
 				<Routes>
 					<Route
-						path="/home"
+						path="/"
 						element={<ComponentsUsage />}
-					/>
-					<Route
-						path="/login"
-						element={<Login />}
-					/>
-					<Route
-						path="/register"
-						element={<Register />}
 					/>
 				</Routes>
 			</BrowserRouter>
