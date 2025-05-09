441b9e28e5e54f611e59b518afd9e7cd48322c75
Geovani Dos Santos Vargas <geovani.020204@gmail.com>
2025-05-08 20:34:08 -0400

    app.tsx merge conflicts fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import SideBar from './components/SideBar'
-
-//pages
 import ComponentsUsage from './pages/ComponentsUsage'
-import Home from './pages/Home'
 
 function App() {
 	return (
@@ -14,10 +11,6 @@ function App() {
 				<Routes>
 					<Route
 						path="/"
-						element={<Home />}
-					/>
-					<Route
-						path="/componentes"
 						element={<ComponentsUsage />}
 					/>
 				</Routes>
